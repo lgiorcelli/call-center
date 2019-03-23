4989f7617487df5b0c2a20c6f139ea18195d830a
Leonardo Giorcelli <leonardo.giorcelli@etermax.com>
2019-03-23 09:28:17 -0300

    Refactor luego del segundo caso

--- src/test/java/com/group/callcenter/DispatcherTest.java
+++ src/test/java/com/group/callcenter/DispatcherTest.java
@@ -1,9 +1,8 @@
 package com.group.callcenter;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -24,7 +23,6 @@ public class DispatcherTest {
 	@Before
 	public void setUp() {
 		operatorPool = mock(Pool.class);
-
 		supervisorPool = mock(Pool.class);
 		managerPool = mock(Pool.class);
 	}
@@ -32,26 +30,36 @@ public class DispatcherTest {
 	@Test
 	public void assign_a_call_to_a_operator_as_a_first_option() {
 		//GIVEN
-		when(operatorPool.assignCall(ANY_CALL)).thenReturn(true);
+		givenAnAvailableGroupOfOperators();
 		givenADispatcherWithRolePriority();
 		//WHEN
-		dispatcher.dispatchCall(ANY_CALL);
+		whenACallIsDispatched();
 		//THEN
-		thenOnlyOperatorsAttendsTheCall();
+		thenOperatorsAttendsTheCall();
 	}
 
 	@Test
-	public void assign_a_call_to_a_operator_when_there_are_not_available_operators() {
+	public void assign_a_call_to_a_supervisor_when_there_are_not_available_operators() {
 		//GIVEN
-		when(operatorPool.assignCall(ANY_CALL)).thenReturn(false);
-		when(supervisorPool.assignCall(ANY_CALL)).thenReturn(true);
+		givenAnUnavailableGroupOfOperators();
+		givenAnAvailableGroupOfSupervisors();
 		givenADispatcherWithRolePriority();
 		//WHEN
-		dispatcher.dispatchCall(ANY_CALL);
+		whenACallIsDispatched();
 		//THEN
-		verify(operatorPool).assignCall(ANY_CALL);
-		verify(supervisorPool).assignCall(ANY_CALL);
-		verifyZeroInteractions(managerPool);
+		thenSupervisorsAttendsTheCall();
+	}
+
+	private void givenAnAvailableGroupOfOperators() {
+		when(operatorPool.canAttendCall()).thenReturn(true);
+	}
+
+	private void givenAnAvailableGroupOfSupervisors() {
+		when(supervisorPool.canAttendCall()).thenReturn(true);
+	}
+
+	private void givenAnUnavailableGroupOfOperators() {
+		when(operatorPool.canAttendCall()).thenReturn(false);
 	}
 
 	private void givenADispatcherWithRolePriority() {
@@ -59,25 +67,51 @@ public class DispatcherTest {
 		dispatcher = new RolePriorityDispatcher(priorityQueue);
 	}
 
-	private void thenOnlyOperatorsAttendsTheCall() {
-		verify(operatorPool).assignCall(ANY_CALL);
-		verifyZeroInteractions(supervisorPool, managerPool);
+	private void whenACallIsDispatched() {
+		dispatcher.dispatchCall(ANY_CALL);
+	}
+
+	private void thenOperatorsAttendsTheCall() {
+		verifyCallsAttended(1, 0);
+	}
+
+	private void thenSupervisorsAttendsTheCall() {
+		verifyCallsAttended(0, 1);
+	}
+
+	private void verifyCallsAttended(int operatorAttendedCalls, int supervisorAttendedCalls) {
+		verify(operatorPool, times(operatorAttendedCalls)).assignCall(ANY_CALL);
+		verify(supervisorPool, times(supervisorAttendedCalls)).assignCall(ANY_CALL);
+		verify(managerPool, times(0)).assignCall(ANY_CALL);
 	}
 
 }
 
 class RolePriorityDispatcher implements Dispatcher {
-	private List<Pool> priorityQueue = Lists.newArrayList();
+	private RolePriorityChain rolePriorityChain;
 
 	public RolePriorityDispatcher(List<Pool> priorityQueue) {
+		rolePriorityChain = new RolePriorityChain(priorityQueue);
+	}
+
+	public void dispatchCall(Call call) {
+		rolePriorityChain.dispatchCall(call);
+	}
+}
+
+class RolePriorityChain {
+	private List<Pool> priorityQueue = Lists.newArrayList();
+
+	public RolePriorityChain(List<Pool> priorityQueue) {
 		this.priorityQueue.addAll(priorityQueue);
 	}
 
 	public void dispatchCall(Call call) {
 		int priority = 0;
-		while (!priorityQueue.get(priority).assignCall(call)) {
+		while (!priorityQueue.get(priority).canAttendCall()) {
 			priority++;
 		}
+		priorityQueue.get(priority).assignCall(call);
 	}
 }
 
@@ -86,6 +120,7 @@ interface Dispatcher {
 }
 
 interface Pool {
+	boolean canAttendCall();
 
 	boolean assignCall(Call call);
 }
