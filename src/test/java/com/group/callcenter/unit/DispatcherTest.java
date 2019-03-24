1da4c6b2329aa84162c45713a539cf9845619ed5
Leonardo Giorcelli <leonardo.giorcelli@etermax.com>
2019-03-24 09:48:45 -0300

    Se verifica la invocacion del handler cuando el dispatcher excede su capacidad de procesamiento

--- src/test/java/com/group/callcenter/unit/DispatcherTest.java
+++ src/test/java/com/group/callcenter/unit/DispatcherTest.java
@@ -1,35 +1,65 @@
 package com.group.callcenter.unit;
 
+import java.util.function.Consumer;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import com.group.callcenter.domain.Call;
 import com.group.callcenter.domain.priority.PriorityChainHandler;
 
 public class DispatcherTest {
-	private Call call = () -> { };
+	private Call call = () -> {
+	};
+	private Consumer<Call> onDispatcherCapacityExceeded;
+	private PriorityChainHandler priorityHandler;
+
+	@Before
+	public void setUp() {
+		priorityHandler = Mockito.mock(PriorityChainHandler.class);
+		onDispatcherCapacityExceeded = Mockito.mock(Consumer.class);
+	}
 
 	@Test
-	public void call_priority_handler_when_a_call_arrives_and_has_answer_capacity() {
-		PriorityChainHandler priorityHandler = Mockito.mock(PriorityChainHandler.class);
-
-		Dispatcher dispatcher = new Dispatcher(priorityHandler);
+	public void call_PriorityHandler_when_a_call_arrives_and_has_answer_capacity() {
+		Dispatcher dispatcher = new Dispatcher(priorityHandler, onDispatcherCapacityExceeded, 10);
 		//WHEN
 		dispatcher.dispatchCall(call);
 		//THEN
 		Mockito.verify(priorityHandler).handle(call);
+		Mockito.verifyZeroInteractions(priorityHandler);
+	}
+
+	@Test
+	public void handle_a_call_when_exceeds_answer_capacity() {
+		//GIVEN
+		Dispatcher dispatcher = new Dispatcher(priorityHandler, onDispatcherCapacityExceeded, 0);
+		//WHEN
+		dispatcher.dispatchCall(call);
+		//THEN
+		Mockito.verify(onDispatcherCapacityExceeded).accept(call);
+		Mockito.verifyZeroInteractions(priorityHandler);
 	}
 
 	public class Dispatcher {
 		private PriorityChainHandler priorityHandler;
+		private Consumer<Call> onDispatcherCapacityExceeded;
+		private int currentOngoingCalls = 0;
+		private int maxOngoingCalls;
 
-		public Dispatcher(PriorityChainHandler priorityHandler) {
+		public Dispatcher(PriorityChainHandler priorityHandler, Consumer<Call> onDispatcherCapacityExceeded, int maxOngoingCalls) {
 			this.priorityHandler = priorityHandler;
+			this.onDispatcherCapacityExceeded = onDispatcherCapacityExceeded;
+			this.maxOngoingCalls = maxOngoingCalls;
 		}
 
 		void dispatchCall(Call call) {
-			priorityHandler.handle(call);
+			if (currentOngoingCalls >= maxOngoingCalls) {
+				onDispatcherCapacityExceeded.accept(call);
+			} else {
+				priorityHandler.handle(call);
+			}
 		}
 	}
 }
